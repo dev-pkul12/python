cfe673b550b649a2a7dc4cea40bdf4bb9465ad79
dev-pkul12 <prasannakulkarni2020@gmail.com>
2025-08-16 17:52:43 +0530

    operators added

---
+++ basics/operators.py
@@ -0,0 +1,53 @@
+# 1. Arithmetic Operators
+# + (Addition) → 5 + 3 = 8
+# - (Subtraction) → 10 - 4 = 6
+# * (Multiplication) → 6 * 2 = 12
+# / (Division) → 15 / 2 = 7.5
+#  (Floor Division) → 15 // 2 = 7
+# % (Modulus) → 15 % 2 = 1
+# ** (Exponentiation) → 2 ** 3 = 8
+
+
+# 2. Comparison (Relational) Operators
+# == (Equal) → 5 == 5 → True
+# (Not Equal) → 5 != 3 → True
+# > (Greater Than) → 10 > 3 → True
+# < (Less Than) → 2 < 8 → True
+# >= (Greater or Equal) → 7 >= 7 → True
+# <= (Less or Equal) → 4 <= 6 → True
+
+
+# 3. Assignment Operators
+# = (Assign) → x = 10
+# += (Add and assign) → x = 5 → x += 3 → x = 8
+# -= (Subtract and assign) → x = 5 → x -= 2 → x = 3
+# *= (Multiply and assign) → x = 4 → x *= 3 → x = 12
+# /= (Divide and assign) → x = 10 → x /= 2 → x = 5.0
+# (Floor divide and assign) → x = 10 → x //= 3 → x = 1
+# %= (Modulo and assign) → x = 10 → x %= 3 → x = 1
+# **= (Power and assign) → x = 2 → x **= 3 → x = 8
+
+
+# 4. Logical Operators
+# and → (5 > 2) and (3 < 4) → True
+# or → (5 > 10) or (4 == 4) → True
+# not → not (5 > 2) → False
+
+
+# 5. Identity Operators
+# is → x = [1,2]; y = x → x is y → True
+# is not → x = [1,2]; y = [1,2] → x is not y → True
+
+
+# 6. Membership Operators
+# in → "a" in "apple" → True
+# not in → "z" not in "apple" → True
+
+
+# 7. Bitwise Operators
+# & (AND) → 5 & 3 → 1
+# | (OR) → 5 | 3 → 7
+# ^ (XOR) → 5 ^ 3 → 6
+# ~ (NOT) → ~5 → -6
+# << (Left Shift) → 5 << 1 → 10
+# >> (Right Shift) → 5 >> 1 → 2
